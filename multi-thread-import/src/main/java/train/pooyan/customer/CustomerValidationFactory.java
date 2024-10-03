c2f07cf935703e04b7b1128afd1dcb060eda2d78
Amirhosein PooyanMoghadam <103578053+AmirhoseinPM@users.noreply.github.com>
2024-10-03 18:10:02 +0330

    Update CustomerValidationFactory.java

--- multi-thread-import/src/main/java/train/pooyan/customer/CustomerValidationFactory.java
+++ multi-thread-import/src/main/java/train/pooyan/customer/CustomerValidationFactory.java
@@ -3,11 +3,12 @@ package train.pooyan.customer;
 import org.springframework.stereotype.Component;
 
 import train.pooyan.core.EntityValidation;
+import train.pooyan.core.ValidationFactory;
 
 @Component
-public class CustomerValidationFactory {
+public class CustomerValidationFactory implements ValidationFactory<Customer> {
 
-	public static EntityValidation<Customer> getCustomerValidation(String line) {
+	public EntityValidation<Customer> getValidation(String line) {
 		return new CustomerValidation(line);
 	}
 }
