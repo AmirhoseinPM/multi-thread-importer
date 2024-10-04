c17a19d9a9c646071c8695bfc89cda89b989129b
Amirhosein PooyanMoghadam <103578053+AmirhoseinPM@users.noreply.github.com>
2024-10-04 11:39:25 +0330

    add comments

--- multi-thread-import/src/main/java/train/pooyan/core/MyExecutorService.java
+++ multi-thread-import/src/main/java/train/pooyan/core/MyExecutorService.java
@@ -1,28 +1,24 @@
 package train.pooyan.core;
 
+import org.springframework.stereotype.Component;
+
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-
+@Component
 public class MyExecutorService {
-	
-	private static ExecutorService executorService;
-	
-	private MyExecutorService() {
 
+	private ExecutorService executorService;
+
+	private MyExecutorService() {
 	}
 
-	public static ExecutorService getExecutorSercvice() {
+	public ExecutorService getExecutorSercvice() {
+		// get a singleton instance of executor for doing tasks concurrently in main app
 		if (executorService == null)
 			executorService = Executors.newScheduledThreadPool(
 					Runtime.getRuntime().availableProcessors()
 			);
 		return executorService;
-	}
-
-
-	
-	public void shutdown() {
-		executorService.shutdown();
 	}
 }
