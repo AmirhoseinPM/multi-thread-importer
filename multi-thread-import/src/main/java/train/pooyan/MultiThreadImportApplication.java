c1e23dcc3964771581bf6bea1f04eb22a612b7b3
Amirhosein PooyanMoghadam <103578053+AmirhoseinPM@users.noreply.github.com>
2024-10-04 11:37:25 +0330

    add comments

--- multi-thread-import/src/main/java/train/pooyan/MultiThreadImportApplication.java
+++ multi-thread-import/src/main/java/train/pooyan/MultiThreadImportApplication.java
@@ -3,10 +3,8 @@ package train.pooyan;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -21,47 +19,45 @@ import train.pooyan.error.ErrorWriter;
 @PropertySource("classpath:application.properties")
 public class MultiThreadImportApplication {
 
-
-	private static final Logger log = LoggerFactory.getLogger(MultiThreadImportApplication.class);
-
-	@Autowired
-	private static MyExecutorService executorService;
-	
-	
-	@Autowired
-	private static ErrorWriter errorWriter;
+	private static final Logger log =
+			LoggerFactory.getLogger(MultiThreadImportApplication.class);
 
 	public static void main(String[] args) {
 		ApplicationContext context = SpringApplication.run(MultiThreadImportApplication.class, args);
 
+		// Reading and processing Bean, for customer and account files
 		CustomerFileReader customerFileReader = context.getBean(CustomerFileReader.class);
 		AccountFileReader accountFileReader = context.getBean(AccountFileReader.class);
-		ErrorWriter errorWriter = context.getBean(ErrorWriter.class);
 
-		ExecutorService executor = executorService.getExecutorSercvice();
-		
-		
+		// get Singleton executor instance for do tasks concurrently
+		MyExecutorService myExecutorService = context.getBean(MyExecutorService.class);
+		ExecutorService executor = myExecutorService.getExecutorSercvice();
+
+		// track completing tasks that we add to executor
 		CountDownLatch threadCount = new CountDownLatch(2);
 
+		// start processing of account csv file
 		executor.submit(
 				() -> accountFileReader.start(threadCount)
 		);
+
+		// start processing of customer csv file
 		executor.submit(
 				() -> customerFileReader.start(threadCount)
 		);
-		
+
 		try {
+			// waiting for all tasks done
 			threadCount.await();
 		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} finally {
+			// shutdown executor after all tasks done
 			executor.shutdown();
+			// write error that saved in ErrorWriter bean to errors file
+			ErrorWriter errorWriter = context.getBean(ErrorWriter.class);
 			errorWriter.writeErrors();
 			log.info("End----------------------------");
 		}
-		
-
-		
 	}
 }
