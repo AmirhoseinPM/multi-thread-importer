a3006c1bb9b36fde73055e11b52aaf22f0373f3e
Amirhosein PooyanMoghadam <103578053+AmirhoseinPM@users.noreply.github.com>
2024-10-04 12:09:10 +0330

    add comments

--- multi-thread-import/src/main/java/train/pooyan/core/LineProcessor.java
+++ multi-thread-import/src/main/java/train/pooyan/core/LineProcessor.java
@@ -2,23 +2,29 @@ package train.pooyan.core;
 
 import java.time.LocalDate;
 
-import train.pooyan.customer.Customer;
 import train.pooyan.error.Error;
 import train.pooyan.error.ErrorWriter;
 
 public abstract class LineProcessor<T> {
-	
-	
-
+	/*
+	 * this class implement core functionality of processing each line of a csv files,
+	 * that map to entity of <T>.
+	 * to doing that
+	 * 1. first validate entity using an EntityValidation<T>
+	 * 2. then
+	 * 		if validated, insert entity it to database,
+	 * 		otherwise write an error to json file.
+	 * */
 	public abstract EntityValidation<T> getValidation(String line);
 	public abstract String getFileName();
 	public abstract ErrorWriter getErrorWriter();
 	public abstract T saveEntity(T entity);
-	
-	
+
 	public void processLine(String line) {
-        
-        EntityValidation<T> validation = getValidation(line);        
+		// process line and validate and maps it to entity
+        EntityValidation<T> validation = getValidation(line);
+
+		// insert entity to database or write to json
         importOrError(validation); 
     }
 	
@@ -29,24 +35,19 @@ public abstract class LineProcessor<T> {
 	        else {
 	            try {
 	                saveEntity(
-	                		(T) validation.getValidated());
+	                		validation.getValidated());
 	            } catch (Exception exception) {
 	                errorHandling(validation, "500", "SQL", 
 	                			"data base exception on saving record");
 	            }
 	        }
-	    }
-	    
-	
-    public void errorHandling(EntityValidation validation, String code, String classification, String discription) {
+	 }
+
+    public void errorHandling(EntityValidation<T> validation, String code, String classification, String discription) {
         Error error = new Error(
-                getFileName(),
-                validation.getRecordNumber(),
-                code,
-                classification,
-                discription,
-                LocalDate.now().toString()
-        );
+                getFileName(), validation.getRecordNumber(), code,
+                classification, discription, LocalDate.now().toString());
+
         getErrorWriter().addError(error);
     }
 }
