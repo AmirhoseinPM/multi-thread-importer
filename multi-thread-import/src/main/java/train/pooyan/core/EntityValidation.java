95eb45983d1770399e1e5c393afdbf77a0a21d69
Amirhosein PooyanMoghadam <103578053+AmirhoseinPM@users.noreply.github.com>
2024-10-04 11:56:32 +0330

    add comments

--- multi-thread-import/src/main/java/train/pooyan/core/EntityValidation.java
+++ multi-thread-import/src/main/java/train/pooyan/core/EntityValidation.java
@@ -3,6 +3,10 @@ package train.pooyan.core;
 import java.util.Map;
 
 public interface EntityValidation<T> {
+	/*
+	* this interface shows abstract functionality of
+	* validation classes that created for validate <T> entity
+	* */
 	T getValidated();
 	long getRecordNumber();
 	boolean hasError();
