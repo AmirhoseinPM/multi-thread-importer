b26c4358056041e14baf38d3c3c226002106ae37
Amirhosein PooyanMoghadam <103578053+AmirhoseinPM@users.noreply.github.com>
2024-10-04 18:43:09 +0330

    Update CustomerRepo.java

--- multi-thread-import/src/main/java/train/pooyan/customer/CustomerRepo.java
+++ multi-thread-import/src/main/java/train/pooyan/customer/CustomerRepo.java
@@ -1,18 +1,7 @@
 package train.pooyan.customer;
 
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
-import jakarta.transaction.Transactional;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-@Repository
-@Transactional
-public class CustomerRepo {
+public interface CustomerRepo extends JpaRepository<Customer, Long> {
 
-    @PersistenceContext
-    EntityManager entityManager;
-
-    public void save(Customer item) {
-        entityManager.persist(item);
-    }
 }
